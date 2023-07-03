c08cb704a6acdaa32288ec0265e3f5d20004c4a4
LENOVO LEGION <LENOVO LEGION@DESKTOP-OK2JDO8>
2023-07-02 22:19:38 -0500

    Agregar archivos de proyecto.

---
+++ mgonzalezdeberS5/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mgonzalezdeberS5
+{
+    public partial class MainPage : ContentPage
+    {
+        //Variables globales para el url, cliente y donde almacenamos temporalmente
+        private string url = "http://192.168.100.62/ws_PanFrances/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Usuario> post;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cargarDatos();
+        }
+
+
+        public async void cargarDatos()
+        {
+            var contenido = await cliente.GetStringAsync(url);
+            List<Usuario> listaPost = JsonConvert.DeserializeObject<List<Usuario>>(contenido);
+            post = new ObservableCollection<Usuario>(listaPost);
+            listaUsuarios.ItemsSource = post;
+        }
+    }
+}
---
+++ mgonzalezdeberS5/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mgonzalezdeberS5
+{
+    internal class Usuario
+    {
+        public int userID { set; get; }
+
+        public string nombre { set; get; }
+
+        public string apellido { set; get; }
+
+        public string correo { set; get; }
+    }
+}
